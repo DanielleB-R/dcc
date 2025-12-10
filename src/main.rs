a278aa30ce80a10c45f14f0123c39559e94bdfc3
Danielle Brook-Roberge <danielle@brook-roberge.ca>
2025-12-10 13:00:16 -0800

    Use clap for optimization options

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,50 @@ fn compiler_stage(args: &StageArgs) -> Stage {
     }
 }
 
+#[derive(Debug, Args)]
+#[group(required = false, multiple = true)]
+struct OptimizationArgs {
+    #[arg(long)]
+    fold_constants: bool,
+
+    #[arg(long)]
+    propagate_copies: bool,
+
+    #[arg(long)]
+    eliminate_unreachable_code: bool,
+
+    #[arg(long)]
+    eliminate_dead_stores: bool,
+
+    #[arg(long, short = 'O')]
+    optimize: bool,
+}
+
+fn compiler_optimizations(args: &OptimizationArgs) -> OptimizationPasses {
+    let mut passes = OptimizationPasses::default();
+
+    if args.fold_constants {
+        passes.constant_folding = true;
+    }
+    if args.propagate_copies {
+        passes.copy_propagation = true;
+    }
+    if args.eliminate_unreachable_code {
+        passes.unreachable_code_elimination = true;
+    }
+    if args.eliminate_dead_stores {
+        passes.dead_store_elimination = true;
+    }
+    if args.optimize {
+        passes.constant_folding = true;
+        passes.copy_propagation = true;
+        passes.unreachable_code_elimination = true;
+        passes.dead_store_elimination = true;
+    }
+
+    passes
+}
+
 #[derive(Debug, Parser)]
 struct Options {
     #[command(flatten)]
@@ -48,8 +92,8 @@ struct Options {
     #[arg(long)]
     debug: bool,
 
-    #[arg(long)]
-    optimize: Option<String>,
+    #[command(flatten)]
+    optimize_args: OptimizationArgs,
 }
 
 fn main() -> Result<(), CompilerError> {
@@ -58,22 +102,7 @@ fn main() -> Result<(), CompilerError> {
     let source_name = args.source_name;
     let stage = compiler_stage(&args.stage_args);
     let mut debug = args.debug;
-    let mut optimization_passes: OptimizationPasses = Default::default();
-
-    if let Some(o) = args.optimize {
-        if o.contains("fold") {
-            optimization_passes.constant_folding = true;
-        }
-        if o.contains("unreachable") {
-            optimization_passes.unreachable_code_elimination = true;
-        }
-        if o.contains("propagate") {
-            optimization_passes.copy_propagation = true;
-        }
-        if o.contains("dead-stores") {
-            optimization_passes.dead_store_elimination = true;
-        }
-    }
+    let optimization_passes = compiler_optimizations(&args.optimize_args);
 
     let debug_var = env::var("DEBUG");
     if debug_var.is_ok_and(|s| s != "0") {
